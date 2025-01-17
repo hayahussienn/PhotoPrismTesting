ff4859e37c161be5327595a3d6fec6ebf8be1724
hayahussienn <hussienhaya99@gmail.com>
2025-01-17 17:30:03 +0200

    use Ngrok

--- src/test/java/UITests/FilteringTest.java
+++ src/test/java/UITests/FilteringTest.java
@@ -4,12 +4,18 @@ import org.example.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.example.PhotoCollectionUtils;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
+import java.time.Duration;
 import java.util.List;
 
 import static org.example.DriverFactory.getDriver;
@@ -18,7 +24,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FilteringTest
 {
     WebDriver driver;
-    private static final String baseURL = "http://localhost:2342/library/browse";
+    private static final String baseURL = "https://d0af-2a06-c701-9dff-a900-a12b-6398-7b7-8d4e.ngrok-free.app";
     private LoginPage login;
     private HomePage homePage;
 
@@ -52,6 +58,15 @@ public class FilteringTest
     {
         driver = getDriver();
         driver.get(baseURL);
+
+        try {
+            Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+            WebElement visitSiteButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Visit Site']")));
+            visitSiteButton.click();
+        } catch (TimeoutException err) {
+            System.out.println("Ngrok warning page was not loaded");
+        }
+
         login = new LoginPage(driver);
         homePage = login.signInAsValidUser("admin", "yourpassword"); // Navigate to home page after login
     }
--- src/test/java/UITests/LoginTest.java
+++ src/test/java/UITests/LoginTest.java
@@ -5,8 +5,16 @@ import org.example.LoginPage;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 import static org.example.DriverFactory.getDriver;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -14,7 +22,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class LoginTest
 {
     WebDriver driver;
-    private static final String baseURL = "http://localhost:2342/library/browse";
+    private static final String baseURL = "https://d0af-2a06-c701-9dff-a900-a12b-6398-7b7-8d4e.ngrok-free.app";
     private LoginPage login;
 
 
@@ -23,6 +31,15 @@ public class LoginTest
     {
         driver = getDriver();
         driver.get(baseURL);
+
+        try {
+            Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+            WebElement visitSiteButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Visit Site']")));
+            visitSiteButton.click();
+        } catch (TimeoutException err) {
+            System.out.println("Ngrok warning page was not loaded");
+        }
+
         login = new LoginPage(driver);
 
     }
